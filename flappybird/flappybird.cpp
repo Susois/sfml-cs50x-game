9e04dd56a7750003db199675b5a58463a2906234
Susois <suoffice.it@gmail.com>
2025-01-08 14:08:39 +0700

    update

--- flappybird/flappybird.cpp
+++ flappybird/flappybird.cpp
@@ -11,15 +11,8 @@
 const int WINDOW_WIDTH = 1000;
 const int WINDOW_HEIGHT = 512;
 
-//VIRTUAL RESOLUTION 
-const int VIRTUAL_WIDTH = 512;
-const int VIRTUAL_HEIGHT = 288;
-
 //BACKGROUND SCROLL 
-const int backgound_scroll = 0;
-const int ground_scroll = 0;
 const int background_scroll_speed = 60;
-const int ground_scroll_speed = 30;
 // bird
 const float GRAVITY = 800.f;
 const float JUMP_VELOCITY = -300.f;
@@ -33,18 +26,136 @@ const float PIPE_SPAWN_INTERVAL = 1.5f;
 //background
 class background {
 private:
-    sf::Sprite backgroundsprite;
+    sf::Sprite backgroundsprite1;
+    sf::Sprite backgroundsprite2;
     sf::Texture backgroundtexture;
 public:
     background(const std::string& texturefile) {
         backgroundtexture.loadFromFile(texturefile);
-        backgroundsprite.setTexture(backgroundtexture);
+        backgroundsprite1.setTexture(backgroundtexture);
+        backgroundsprite2.setTexture(backgroundtexture);
+        //set background to ngang với cửa sổ window
+        
+        // lấy kích thước cửa sổ window
+        sf::Vector2u texture = backgroundtexture.getSize();
+
+        //scale để vừa với cửa sổ
+        float scalex = static_cast<float>(WINDOW_WIDTH) / texture.x;
+        float scaley = static_cast<float>(WINDOW_HEIGHT) / texture.y;
+
+        backgroundsprite1.setScale(scalex, scaley);
+        backgroundsprite2.setScale(scalex, scaley);
+
+        backgroundsprite1.setPosition(scalex, scaley);
+        backgroundsprite2.setPosition(scalex, scaley);
+
+        // đặt vị trí 2 sprite liền kề nhau
+
+        backgroundsprite1.setPosition(0, 0);
+        backgroundsprite2.setPosition(static_cast<float>(texture.x) * scalex, 0);
+
     }
+    
+    /*void update(float dt) {
+        backgroundsprite.move( -background_scroll_speed * dt ,0);
+    }
+    void render(sf::RenderWindow& window) {
+        window.draw(backgroundsprite);
+    }
+    void resetposition(float startx, float starty) {
+        backgroundsprite.setPosition(startx, starty);
+    }
+    sf::FloatRect getbound()const {
+        return backgroundsprite.getGlobalBounds();
+    }*/
 
     void update(float dt) {
-        backgroundsprite.move(0, -background_scroll_speed);
+        // di chuyen 2 sprite sang trai
+        float movement = -background_scroll_speed * dt;
+        backgroundsprite1.move(movement, 0);
+        backgroundsprite2.move(movement, 0);
+
+        // kiem tra neu sprite ra khoi man hinh, dat lai vi tri
+        if (backgroundsprite1.getPosition().x + backgroundsprite1.getGlobalBounds().width <= 0) {
+            backgroundsprite1.setPosition(
+                backgroundsprite2.getPosition().x + backgroundsprite2.getGlobalBounds().width, 0
+            );
+        }
+        if (backgroundsprite2.getPosition().x + backgroundsprite2.getGlobalBounds().width <= 0) {
+            backgroundsprite2.setPosition(
+                backgroundsprite1.getPosition().x + backgroundsprite1.getGlobalBounds().width, 0
+            );
+        }
+    }
+    void render(sf::RenderWindow& window) {
+        window.draw(backgroundsprite1);
+        window.draw(backgroundsprite2);
     }
 
+    void resetposition() {
+        backgroundsprite1.setPosition(0, 0);
+        backgroundsprite2.setPosition(backgroundsprite1.getGlobalBounds().width, 0);
+    }
+
+};
+
+//ground
+class ground {
+private: 
+    sf::Texture groundtexture;
+    sf::Sprite groundsprite1;
+    sf::Sprite groundsprite2;
+public:
+    ground(const sf::String& texturefile) {
+        groundtexture.loadFromFile(texturefile);
+        groundsprite1.setTexture(groundtexture);
+        groundsprite2.setTexture(groundtexture);
+
+        // lay kich thuoc cua so window
+        sf::Vector2u texture = groundtexture.getSize();
+
+        //lay scale
+        //float scalex = static_cast<float>(WINDOW_WIDTH) / texture.x;
+        float scalex = static_cast<float>(WINDOW_WIDTH) / texture.x;
+
+        float scaley = texture.y /3;
+        
+
+        groundsprite1.setScale(scalex, scaley);
+        groundsprite2.setScale(scalex, scaley);
+
+        groundsprite1.setPosition(scalex, scaley);
+        groundsprite2.setPosition(scalex, scaley);
+
+        groundsprite1.setPosition(0,WINDOW_HEIGHT * 6/7);
+        groundsprite2.setPosition(static_cast<float>(texture.x) * scalex, WINDOW_HEIGHT * 6 / 7 );
+        //groundsprite2.setPosition(static_cast<float>(texture.x) * 1, WINDOW_HEIGHT / 2);
+
+    }
+    void update(float dt) {
+        float movement = -background_scroll_speed * dt;
+        groundsprite1.move(movement, WINDOW_HEIGHT*6/7);
+        groundsprite2.move(movement, WINDOW_HEIGHT*6/7);
+
+        if (groundsprite1.getPosition().x + groundsprite1.getGlobalBounds().width <= 0) {
+            groundsprite1.setPosition(
+                groundsprite2.getPosition().x + groundsprite2.getGlobalBounds().width, 0);
+        }
+
+        if (groundsprite2.getPosition().x + groundsprite2.getGlobalBounds().width <= 0) {
+            groundsprite2.setPosition(
+                groundsprite2.getPosition().x + groundsprite2.getGlobalBounds().width, 0);
+        }
+        
+    }
+    void render(sf::RenderWindow& window) {
+        window.draw(groundsprite1);
+        window.draw(groundsprite2);
+    }
+    void resetpositon() {
+        groundsprite1.setPosition(0, WINDOW_HEIGHT * 6/7);
+        groundsprite2.setPosition(groundsprite1.getGlobalBounds().width, WINDOW_HEIGHT*6/7);
+    }
 };
 
 // getrandom
@@ -140,8 +251,8 @@ public:
     PipePair(sf::Texture& texture, float startX, float gapY, float gapHeight)
         /*: topPipe("data/pipe.png", startX, gapY - gapHeight / 2, true),
         bottomPipe("data/pipe.png", startX, gapY + gapHeight /2 , false),*/
-        : topPipe(texture, startX, gapY - gapHeight/2, true),
-        bottomPipe(texture,startX, gapY + gapHeight/2, false),
+        : topPipe(texture, startX, gapY - gapHeight/3, true),
+        bottomPipe(texture,startX, gapY + gapHeight/3, false),
         scored(false) {}
 
     void update(float dt) {
@@ -189,15 +300,19 @@ private:
     sf::Text scoreText;
     sf::Texture pipetexture;
     sf::Texture background_texture;
+    sf::Texture ground_texture;
+    background back_ground;
+    ground ground_;
 public:
     Game()
-        : window(sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT }), "Flappy Bird", sf::Style::Close),
-        bird("data/bird.png", 100, WINDOW_HEIGHT / 2),
-        timeSinceLastPipe(0),
-        score(0){
+            : window(sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT }), "Flappy Bird", sf::Style::Close),
+            back_ground("data/background.png"),
+            ground_("data/ground.png"),
+            bird("data/bird.png", 100, WINDOW_HEIGHT / 2),
+            timeSinceLastPipe(0),
+            score(0){
         window.setFramerateLimit(60);
         pipetexture.loadFromFile("data/pipe.png");
-        background_texture.loadFromFile("data/background.png");
         font.loadFromFile("data/font.ttf");
         scoreText.setFont(font);
         scoreText.setCharacterSize(32);
@@ -229,6 +344,7 @@ protected:
     void update() {
         float dt = clock.restart().asSeconds();
         bird.update(dt);
+        back_ground.update(dt);
 
         // Update pipes
         timeSinceLastPipe += dt;
@@ -238,6 +354,11 @@ protected:
             pipes.emplace_back(pipetexture, WINDOW_WIDTH, gapY, 200);
             timeSinceLastPipe = 0;
         }
+
+        //if (back_ground.getbound().left + back_ground.getbound().width <= window.getSize().x) {
+        //    back_ground.resetposition(0, 0);
+        //}
+
 
         for (auto& pipe : pipes) {
             pipe.update(dt);
@@ -251,8 +372,9 @@ protected:
             pipes.end());
 
         // Check collisions
+        // vòng for cho dành cho tất cả pipe đang chạy
         for (auto& pipe : pipes) {
-            if (pipe.collidesWith(bird)) {
+            if (pipe.collidesWith(bird)) { // kiểm tra va chạm giữa bird và pipe, trả về true nếu có va chạm
                 resetGame();
                 return;
             }
@@ -263,13 +385,19 @@ protected:
             }
         }
 
+        // check nếu bird ở ngoài màn hìnưind
+        if (bird.getBounds().top < 0 || bird.getBounds().top + bird.getBounds().height >= window.getSize().y) {
+            resetGame();
+            return;
+        }
         // Update score text
         scoreText.setString("Score: " + std::to_string(score));
     }
 
     void render() {
         window.clear();
-
+        back_ground.render(window);
+        ground_.render(window);
         bird.render(window);
         for (auto& pipe : pipes) {
             pipe.render(window);
@@ -281,6 +409,8 @@ protected:
 
     void resetGame() {
         bird.resetPosition(100, WINDOW_HEIGHT / 2);
+        back_ground.resetposition();
+        ground_.resetpositon();
         pipes.clear();
         score = 0;
     }
